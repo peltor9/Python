8bb654c11359e9708b613bd2bf29e5cc67238d82
peltor9 <47584184+peltor9@users.noreply.github.com>
2021-01-20 18:36:06 +0700

    Create Array_3D.py

---
+++ Array_3D.py
@@ -0,0 +1,11 @@
+## Print array
+for i in range(4):
+     for j in range(5):
+          for k in range(3):
+              print(i,j,k)
+
+## Reverse print
+for i in range(3):
+     for j in range(5):
+          for k in range(4):
+              print(k,j,i)
